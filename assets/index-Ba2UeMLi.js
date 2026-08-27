f0403d81f84a22e81dfc54fd4a289eeefe044ddf
explainpark101 <explainpark101@users.noreply.github.com>
2026-08-27 06:49:04 +0000

    deploy: 2717ad1289cbf40bea1ac83a7b40125329a84974

---
+++ assets/index-Ba2UeMLi.js
@@ -0,0 +1,15 @@
+import { j as r } from "./index-RydzSnnb.js";
+import "./vendor-react-kfkzeLNk.js";
+import "./vendor-md-editor-BebLMpT_.js";
+import "./vendor-aws-BPUgBAdC.js";
+import "./vendor-lucide-DsWVGDs1.js";
+import "./vendor-zip-Bez6qchM.js";
+import "./vendor-motion-9P87yVtW.js";
+import "./vendor-radix-BWhlk-Y9.js";
+import "./vendor-google-genai-BXoTgYIl.js";
+async function j(o = {}) {
+  return typeof o == "object" && Object.freeze(o), await r("plugin:dialog|open", { options: o });
+}
+export {
+  j as open
+};
