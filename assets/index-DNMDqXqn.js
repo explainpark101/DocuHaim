bc0ba2e8e9049087ba570ba9a479d75f0d3c0d3c
explainpark101 <explainpark101@users.noreply.github.com>
2026-08-29 23:48:15 +0000

    deploy: c351973ddab918a83bf284b6a164b8d46c2b3328

---
+++ assets/index-DNMDqXqn.js
@@ -0,0 +1,15 @@
+import { j as r } from "./index-D0f64dRg.js";
+import "./vendor-react-BFxggocB.js";
+import "./vendor-md-editor-BhV_TtV2.js";
+import "./vendor-aws-CqSEirea.js";
+import "./vendor-lucide-xJOxFrAB.js";
+import "./vendor-zip-Bez6qchM.js";
+import "./vendor-motion-b8oTnHK_.js";
+import "./vendor-radix-D1IYqH78.js";
+import "./vendor-google-genai-DGp6lEvQ.js";
+async function c() {
+  await r("plugin:process|restart");
+}
+export {
+  c as relaunch
+};
---
+++ assets/index-D_9QwlME.js
@@ -0,0 +1,36 @@
+import { j as i } from "./index-D0f64dRg.js";
+import { I as t, t as e } from "./image-BfFs64mV.js";
+import "./vendor-react-BFxggocB.js";
+import "./vendor-md-editor-BhV_TtV2.js";
+import "./vendor-aws-CqSEirea.js";
+import "./vendor-lucide-xJOxFrAB.js";
+import "./vendor-zip-Bez6qchM.js";
+import "./vendor-motion-b8oTnHK_.js";
+import "./vendor-radix-D1IYqH78.js";
+import "./vendor-google-genai-DGp6lEvQ.js";
+async function s(a, r) {
+  await i("plugin:clipboard-manager|write_text", { label: r == null ? void 0 : r.label, text: a });
+}
+async function f() {
+  return await i("plugin:clipboard-manager|read_text");
+}
+async function b(a) {
+  await i("plugin:clipboard-manager|write_image", { image: e(a) });
+}
+async function y() {
+  return await i("plugin:clipboard-manager|read_image").then((a) => new t(a));
+}
+async function x(a, r) {
+  await i("plugin:clipboard-manager|write_html", { html: a, altText: r });
+}
+async function I() {
+  await i("plugin:clipboard-manager|clear");
+}
+export {
+  I as clear,
+  y as readImage,
+  f as readText,
+  x as writeHtml,
+  b as writeImage,
+  s as writeText
+};
