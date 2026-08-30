1e15b69f342a6943bd1c0392b2bbdae70d6056b4
explainpark101 <explainpark101@users.noreply.github.com>
2026-08-30 01:26:09 +0000

    deploy: b8dbb4205941c7deb59dc5e3bd47287661b70176

---
+++ assets/index-DM2W0oLD.js
@@ -0,0 +1,40 @@
+import { j as a } from "./index-DGkRmTW6.js";
+import "./vendor-react-BFxggocB.js";
+import "./vendor-md-editor-CRNS8cBC.js";
+import "./vendor-aws-CacdPxb-.js";
+import "./vendor-lucide-D7vvAA4A.js";
+import "./vendor-zip-Bez6qchM.js";
+import "./vendor-motion-b8oTnHK_.js";
+import "./vendor-radix-ClPHWmRP.js";
+import "./vendor-google-genai-DGp6lEvQ.js";
+var n;
+(function(t) {
+  t[t.None = 0] = "None", t[t.Auto = 1] = "Auto", t[t.TouchID = 2] = "TouchID", t[t.FaceID = 3] = "FaceID", t[t.Iris = 4] = "Iris";
+})(n || (n = {}));
+async function g() {
+  return await a("plugin:biometry|status");
+}
+async function D(t, i = {}) {
+  await a("plugin:biometry|authenticate", { reason: t, options: i });
+}
+async function h(t) {
+  return await a("plugin:biometry|has_data", { options: t });
+}
+async function b(t) {
+  return await a("plugin:biometry|get_data", { options: t });
+}
+async function l(t) {
+  await a("plugin:biometry|set_data", { options: t });
+}
+async function w(t) {
+  await a("plugin:biometry|remove_data", { options: t });
+}
+export {
+  n as BiometryType,
+  D as authenticate,
+  g as checkStatus,
+  b as getData,
+  h as hasData,
+  w as removeData,
+  l as setData
+};
---
+++ assets/preview-CucMEii9.js
@@ -0,0 +1,14 @@
+import { r as o } from "./vendor-react-BFxggocB.js";
+import { a9 as r } from "./index-DGkRmTW6.js";
+function a() {
+  const [s, n] = o.useState(() => r());
+  return o.useEffect(() => {
+    const c = document.documentElement, e = () => n(r());
+    e();
+    const t = new MutationObserver(e);
+    return t.observe(c, { attributes: true, attributeFilter: ["class"] }), () => t.disconnect();
+  }, []), s;
+}
+export {
+  a as u
+};
